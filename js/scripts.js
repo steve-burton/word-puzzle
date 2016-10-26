ca5160274f449fd8eeb76b76e9f2de539f24bc7a
Steve-Burton <stevevburton@gmail.com>
2016-10-25 19:07:07 -0700

    add final javascript to make the project work

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,28 @@
 $(document).ready(function() {
   $("form#puzzle").submit(function(event) {
 
-    var userSentence = $("input#sentence").val();
-    var sentenceSplit = userSentence.split("");
-    var mixSentence = [];
+    var inputSentence = $("input#sentence").val();
+    var puzzle = insertDashes(inputSentence);
+    var puzzle = puzzle.join(" ");
 
-
-
-    for (var count = 0; count < sentenceSplit.length; count += 1) {
-      if (sentenceSplit[count] === "a") {
-        mixSentence.push("-");
-      }
-
-    }
-console.log(sentenceSplit);
-
-    //
-    // for ( var count = 0; count < items.length; count += 1) {
-    //
-    // groceryList.push(items[count]);
-    //
-    // }
-    //
-    // $("#display").text(groceryList);
-
+    $("span").text(puzzle);
     event.preventDefault();
-
-
   });
 });
 
+var puzzle = [];
+var makeArray = function(sentence) {
+  var sentenceArray = sentence.split("");
+  return sentenceArray;
+}
 
+var insertDashes = function(sentence) {
+  var sentenceArray2 = makeArray(sentence);
 
-    // var userSentence = [$("input#sentence").val()];
-    // console.log(userSentence);
-    // var vowels = ["aeiouy"];
+    for (var count = 0; count < sentenceArray2.length; count += 1) {
+      if (sentenceArray2[count] === "a" || sentenceArray2[count] === "e" || sentenceArray2[count] === "i" || sentenceArray2[count] === "o" || sentenceArray2[count] === "u") {
+        sentenceArray2[count] = "-";
+      }
+    }
+    return sentenceArray2;
+  }
